35a0196125639134b2029b1112b7cf75a9915d65
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-07-15 22:45:27 +0800

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Claudio Tendean on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
